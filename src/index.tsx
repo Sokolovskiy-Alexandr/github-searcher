111b0e82a89c1a6c4f84684e99cc87f65be2cbb9
Sokolovskiy Alexandr <sokolovkiyalexandr@gmail.com>
2022-09-02 22:36:53 +0300

    Change basename

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <BrowserRouter basename="/React">
+  <BrowserRouter basename="/">
     <Provider store={store}>
       <App />
     </Provider>
